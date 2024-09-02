cb39d132515cde6b2dc094710409761ae83a74e7
suvo <suvodatta72@gmail.com>
2024-09-02 22:51:08 +0600

    toUppercase toLowercase and trime method done

---
+++ intro.js
@@ -0,0 +1,26 @@
+// Note: String is immuteable
+
+
+let country = "Bangaldesh" // typeof string
+let division = 'Khulna'; // typeof string
+let distic = `Jessor`; // typeof string
+let thana = new String("Jessor_sadar"); // typeof = object
+
+
+// console.log(country);
+// country[0] = "V";
+// console.log(country);
+
+// Here string is immuteable
+
+//---------------------------
+
+let ary = [2, 3, 4, 5, 6, 7];
+
+// console.log(ary);
+// ary[0] = 10; 
+// console.log(ary);
+
+// here Array is mutable
+
+
---
+++ uppercase_lowercase_trim.js
@@ -0,0 +1,49 @@
+
+// let text = "loremCillum officia sint deserunt quis consequat"
+
+// string.split()
+// console.log(text.split(" "))
+
+// ---------------------
+
+
+let firstName = "suvo"
+//String.toUpperCase();
+
+// console.log(firstName.toUpperCase());
+// console.log(firstName.toLowerCase());
+
+// Example-1:
+let subj1 = "Mathemetices";
+let subj2 = "mathemetices";
+
+// here M amd m difference for this if condition return false value;
+if(subj1 === subj2){
+    // console.log("I love mathematices")
+} else {
+    // console.log("l like mathe")
+}
+
+// those above codes inside if add toUppercase() or toLowecase
+//------------------------------------------------------------
+
+if(subj1.toLowerCase() === subj2.toLowerCase()) {
+    // console.log("I love mathematices");
+} else {
+    // console.log("I like mathe")
+}
+
+
+
+
+
+// Trim();
+// trim() method remoded around space. not word & letter inside
+
+let nme = " suvo datta              ";
+let nme1 = "           suvo  datta"; // have word inise space 
+
+// console.log(nme.trim())
+// console.log(nme1.trim())
+
+
